5cde7a69e437234979c092240772e531f3069e6a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-05 22:34:16 +0100

    Create Home.jsx

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar, Alert, Slider} from '../Components'
+
+const Home = () => {
+  return (
+    <div>
+          <Alert />
+          <Navbar />
+          <Slider />
+    </div>
+  )
+}
+
+export default Home
