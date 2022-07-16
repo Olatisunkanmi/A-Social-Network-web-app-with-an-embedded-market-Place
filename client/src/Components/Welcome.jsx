a8b8417955e87082c75746cb2cdbf9ba355073d9
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-16 22:34:46 +0100

    Create Welcome.jsx

---
+++ client/src/Components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+              <h1 className='text-5xl font-bold text-red-700'> Welcome </h1>
+    </div>
+  )
+}
+
+export default Welcome
