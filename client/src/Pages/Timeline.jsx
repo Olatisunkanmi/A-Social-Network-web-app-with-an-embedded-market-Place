5eb4944ab030d1bd8674c9dec43e2cc78479e78d
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-17 09:51:26 +0100

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,40 +1,17 @@
-import React from 'react'
-import {  Menu, MessageOutlined, NotificationImportantOutlined, ShoppingCartOutlined } from '@material-ui/icons'
+import React from 'react';
+import { Header, Upload } from '../Components';
+
 
 const Timeline = () => {
+  
+
   return (
-    <div className='bg-stone-200 '>
-
-            <div 
-              className='flex flex-row p-5'>
-                        <div 
-                        className='w-1/5'>
-                            <p> Side bar </p>
-                      </div>
-
-                    <div  className='w-4/5  text-white justify-between'>
-
-                            <div 
-                              className='flex flex-row space-x-1'>
-                            <Menu />
-                            <p> Time Line</p>
-                            </div>
-
-
-                            <div> 
-                                    
-                                    <NotificationImportantOutlined />    
-                                    <MessageOutlined />                             
-                                  <ShoppingCartOutlined />
-                            </div> 
-                      </div>
-
-
+        <div>
+            <Header />
+            <div>
+            <Upload />
             </div>
-
-
-
-    </div>
+        </div>
   )
 }
 
