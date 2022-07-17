b7f07bc909a55491155c986d4adbc5cff6acd8b0
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-17 15:40:16 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserContext, {UserContextProvider} from './Contexts/UserContext';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
@@ -6,9 +7,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
   <BrowserRouter>
-      <App />
+      <UserContextProvider>
+          <App />
+      </UserContextProvider>
     </BrowserRouter>
-  </React.StrictMode>
+ 
 );
