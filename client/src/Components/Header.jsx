445fed70ef31edfa49a2958ada7438b27de88117
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-17 15:40:08 +0100

    Update Header.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,55 +7,61 @@ const Header = () => {
 
     const headerTitle = useLocation().pathname.split('/')[1]
   return (
-    <div className='bg-stone-200 '>
+    <div >
 
-    <div 
-      className='flex flex-row p-5'>
-               
-              <div 
-                className='hidden lg:block lg:w-1/5'>
-                    <p> Side bar </p>
-              </div>
+                <div     
+                className=''>
 
-            <div  className='w-full lg:w-3/5  text-white justify-between flex flex-row'>
-
-                              <div 
-                                className='flex flex-row space-x-1 text-black items-center'>
-                              <Menu />
-                                  <Link to='/timeline'>
-                                  <p  
-                                  className='text-2xl font-serif font-semibold cursor-pointer capitalize '>{headerTitle}</p>
-                                  </Link>
-                              </div>
+                                <div 
+                                    className='flex flex-row p-5'>
 
 
-                              <div
-                                className='text-black space-x-5'> 
-                                      <Favorite className='
-                                      hover:bg-red-600 
-                                      hover:text-white 
-                                      hover:rounded 
-                                       cursor-pointer '/>
-                                      <NotificationImportantOutlined 
-                                      className='hover:bg-sky-600
-                                      hover:text-white
-                                      hover:rounded
-                                      cursor-pointer '/>    
-                                      <MessageOutlined 
-                                      className='hover:bg-sky-600
-                                      hover:text-white
-                                      hover:rounded
-                                      cursor-pointer '/>                             
-                                    <ShoppingCartOutlined 
-                                    className='hover:bg-sky-600
-                                    hover:text-white
-                                    hover:rounded
-                                    cursor-pointer '/>
-                              </div> 
-              </div>
+                                                <div 
+                                                className='hidden lg:block lg:w-1/5'>
+                                                    <p> Side bar </p>
+                                            </div>
+                
+                                                <div  className='w-full lg:w-3/5  text-white justify-between flex flex-row'>
+                                    
+                                                                <div 
+                                                                    className='flex flex-row space-x-1 text-black items-center'>
+                                                                <Menu />
+                                                                    <Link to='/timeline'>
+                                                                    <p  
+                                                                    className='text-2xl font-serif font-semibold cursor-pointer capitalize '>{headerTitle}</p>
+                                                                    </Link>
+                                                                </div>
+                                    
+                                    
+                                                                <div
+                                                                    className='text-black space-x-5'> 
+                                                                        <Favorite className='
+                                                                        hover:bg-red-600 
+                                                                        hover:text-white 
+                                                                        hover:rounded 
+                                                                        cursor-pointer '/>
+                                                                        <NotificationImportantOutlined 
+                                                                        className='hover:bg-sky-600
+                                                                        hover:text-white
+                                                                        hover:rounded
+                                                                        cursor-pointer '/>    
+                                                                        <MessageOutlined 
+                                                                        className='hover:bg-sky-600
+                                                                        hover:text-white
+                                                                        hover:rounded
+                                                                        cursor-pointer '/>                             
+                                                                        <ShoppingCartOutlined 
+                                                                        className='hover:bg-sky-600
+                                                                        hover:text-white
+                                                                        hover:rounded
+                                                                        cursor-pointer '/>
+                                                                </div> 
+                                                </div>
+                
+                                </div>
+                        
 
-
-    </div>
+                </div>
 
 
 
