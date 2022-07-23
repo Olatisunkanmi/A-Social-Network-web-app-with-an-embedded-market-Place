5dece3c9a36a8523c63d80d5dee6297471699a7a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-23 18:11:33 +0100

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Header, Upload } from '../Components';
-import UserContext, {UserContext} from './Components/UserContext';
+// import UserContext, {UserContext} from './Components/UserContext';
 
 
 const Timeline = () => {
-    const [isAuth, setIsAuth] = UserContext(second)
+    // const [isAuth, setIsAuth] = UserContext(second)
 
   return (
         <div 
         className='bg-stone-200 '>
             <Header />
-        
-
-
-
 
          
         </div>
