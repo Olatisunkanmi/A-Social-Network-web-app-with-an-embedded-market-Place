8e19c51116111bbb0ab3aed3999599e485ba3f58
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-24 05:26:03 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import {Home} from './Pages/index'
+import { Welcome, Timeline, Sidebar } from './Components/index'
+
 
 function App() {
   return (
-    <div className="">
-            <Home />
+    <div>
+    
+      <div className='flex flex-row md:p-20 bg-slate-400'>
+        <Sidebar />
+          <Routes>
+            <Route exact path='/timeline' element={ <Timeline />} />
+          </Routes>
+      </div>
     </div>
   );
 }
