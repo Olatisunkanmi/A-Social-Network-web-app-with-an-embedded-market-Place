92f3af22ebd31899e9bc9ab8a8b4fcb50e6be6e1
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-24 11:03:22 +0100

    Update StateContext.js

--- client/src/Contexts/StateContext.js
+++ client/src/Contexts/StateContext.js
@@ -2,13 +2,13 @@ import React, {createContext, useContext, useState } from 'react';
 
 const StateContext = createContext()
 
-// const initialState = {
-//         chat: false,
-//         cart: false,
-//         userProfile: false,
-//         notification: false, 
+const initialState = {
+        chat: false,
+        cart: false,
+        userProfile: false,
+        notification: false, 
 
-// }
+}
 
 export const ContextProvider = ( {  children } ) => {
 
