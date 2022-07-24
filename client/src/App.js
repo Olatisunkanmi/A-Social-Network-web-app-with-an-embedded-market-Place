3bb33e53776dbf13215898738a2bfa819059d139
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-24 11:03:14 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 import {Home} from './Pages/index'
 import { Welcome, Timeline, Sidebar } from './Components/index'
 
 
 function App() {
+  const [sidebar, setSideBar] = useState()
+
   return (
     <div>
     
-      <div className='flex flex-row md:p-20 bg-slate-400'>
+      <div className='flex flex-row  justify-between'>
         <Sidebar />
           <Routes>
             <Route exact path='/timeline' element={ <Timeline />} />
