4256dc011229b7f5dfcff308f10b677ec91ff0f5
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-24 23:21:55 +0100

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import { Welcome, Timeline, Sidebar } from '../Components'
+import { useStateContext } from '../Contexts/Context';
+
 
 const Home = () => {
+      const { sidebar, setSideBar } =  useStateContext();
+        console.log(sidebar)
+
   return (
     <div>
     <Sidebar />
