be0b97fec66641a1fac72b2f988d44d728a0402b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-24 05:26:06 +0100

    Update Header.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import {  Favorite, Menu, MessageOutlined, NotificationImportantOutlined, ShoppingCartOutlined } from '@material-ui/icons'
 import {Link } from 'react-router-dom';
+import { Sidebar } from './index'
 import { useLocation } from 'react-router-dom';
 
 const Header = () => {
@@ -11,71 +12,28 @@ const Header = () => {
 
     const headerTitle = useLocation().pathname.split('/')[1]
   return (
-    <div >
-
-                <div     
-                className=''>
-
-                                <div 
-                                    className='flex flex-row p-5'>
-
-                                     
-                                        <>{sidebar ? console.log('true')
-                                            : <div className='lg:block lg:w-1/5'>
-                                                    <p> Side bar </p>
-                                            </div>
-                
-                                    }
-                                        </>
-                                              
-                                                <div  className='w-full lg:w-3/5  text-white justify-between flex flex-row'>
-                                    
-                                                                <div 
-                                                                    className='flex flex-row space-x-1 text-black items-center'>
-                                                            <button 
-                                                                onClick={() =>  setSideBar(  (prevSideBar) => !prevSideBar) }>
-                                                                <Menu />
-                                                                </button>
-                                                                    <Link to='/timeline'>
-                                                                    <p  
-                                                                    className='text-2xl font-serif font-semibold cursor-pointer capitalize '>{headerTitle}</p>
-                                                                    </Link>
-                                                                </div>
-                                    
-                                    
-                                                                <div
-                                                                    className='text-black space-x-5'> 
-                                                                        <Favorite className='
-                                                                        hover:bg-red-600 
-                                                                        hover:text-white 
-                                                                        hover:rounded 
-                                                                        cursor-pointer '/>
-                                                                        <NotificationImportantOutlined 
-                                                                        className='hover:bg-sky-600
-                                                                        hover:text-white
-                                                                        hover:rounded
-                                                                        cursor-pointer '/>    
-                                                                        <MessageOutlined 
-                                                                        className='hover:bg-sky-600
-                                                                        hover:text-white
-                                                                        hover:rounded
-                                                                        cursor-pointer '/>                             
-                                                                        <ShoppingCartOutlined 
-                                                                        className='hover:bg-sky-600
-                                                                        hover:text-white
-                                                                        hover:rounded
-                                                                        cursor-pointer '/>
-                                                                </div> 
-                                                </div>
-                
-                                </div>
-                        
-
+    <>
+    <div 
+            className='flex flex- row justify-between'>
+                <div 
+                    className='flex flex-row w-1/5'>
+                    <Menu /> 
+                    <p> Timeline </p>
                 </div>
 
 
-
-</div>
+                <div>
+                        <ul
+                         className='flex flex-row w-4/5'>
+                                <li> <MessageOutlined /></li>
+                                <li> <Favorite /> </li> 
+                                <li> <NotificationImportantOutlined/> </li>
+                                <li> <ShoppingCartOutlined /></li>
+                        </ul>
+                </div>
+    </div>
+    </>
+          
   )
 }
 
