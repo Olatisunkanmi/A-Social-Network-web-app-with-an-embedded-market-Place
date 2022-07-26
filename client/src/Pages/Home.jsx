023ecc997c6447286440765cc10db082655bbb08
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-26 03:41:55 +0100

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,14 +5,18 @@ import { useStateContext } from '../Contexts/Context';
 
 
 const Home = () => {
-      const { sidebar, setSideBar } =  useStateContext();
-        console.log(sidebar)
+      const { sideBar, setSideBar } =  useStateContext();
+  console.log(sideBar)
+        
+
+
 
   return (
     <div>
-    <Sidebar />
-          <Routes>
-                <Route path='/timeline' element={ <Timeline />  }/>
+            <Routes>
+                <Route path='/' element={ <Welcome />  }/>
+                <Route path='/settings' element={ <Timeline />  }/>
+
           </Routes>
     </div>
   )
