fe5ab2a24146b5c21acd3728a51fcf094bb3d52e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-26 23:09:41 +0100

    Update index.js

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,3 +3,4 @@ export { default as Timeline } from '../Pages/Timeline';
 export {default as Header } from './Header'
 export {default as Upload } from './Upload'
 export {default as Sidebar} from './Sidebar'
+export {default as Story } from './Story'
