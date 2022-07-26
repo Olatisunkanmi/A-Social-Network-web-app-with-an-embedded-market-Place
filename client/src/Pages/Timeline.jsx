2f78065af480127654c2d06b8d06bc99748c839e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-26 04:34:11 +0100

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header, Upload, Sidebar  } from '../Components';
+import { useStateContext } from '../Contexts/Context';
+import '../App.css'
 
 const Timeline = () => {
-
+  const { sideBar, setSideBar } =  useStateContext();
   return (
         <div 
-        className='bg-stone-200 flex flex-col w-3/4 p-20'>
+        className={` text-white flex flex-col p-20
+        ${sideBar ? 'w-3/4 ' 
+            : 'sidebar-toggle' }`}>
             <Header />
             <p> Hello </p>
           </div>
