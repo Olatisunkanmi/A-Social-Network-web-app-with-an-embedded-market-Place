de5cd558a328f43b070f8d50fd65ae6aa204ddaf
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-26 23:09:29 +0100

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import { Header, Upload  } from '../Components';
+import { Header, Upload, Story  } from '../Components';
 import { useStateContext } from '../Contexts/Context';
 import '../App.css'
 
 const Timeline = () => {
   const { sideBar, setSideBar } =  useStateContext();
   return (
-        <div 
-        className={` text-black flex flex-col p-20
-        md:w-full
-        ${sideBar ? 'w-3/4 ' 
-            : 'sidebar-toggle' }`}>
+    <div 
+    className={` text-black flex flex-col p-20 
+    ${sideBar ? 'w-9/12  ' 
+        : 'w-9/12 m-auto' }`}>
             <Header />
             
-                  <div className=''>
+                  <div className='flex flex-row'>
 
                           {/** Feed Section  */}
                               <div 
-                                className='w-4/6'>
+                                className='w-4/6 mr-4'>
                                   <Upload />
                               </div>
 
@@ -26,7 +25,7 @@ const Timeline = () => {
 
 
                               <div className='w-2/5'>
-                              
+                                  <Story />
                               </div>
                   </div>
           </div>
