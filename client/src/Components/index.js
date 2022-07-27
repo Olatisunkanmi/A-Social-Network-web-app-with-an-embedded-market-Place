470c6f856cf01428778479686f85965a57d72cb9
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-27 15:26:58 +0100

    Update index.js

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -4,4 +4,5 @@ export {default as Header } from './Header'
 export {default as Upload } from './Upload'
 export {default as Sidebar} from './Sidebar'
 export {default as Story } from './Story'
-export {default as Random } from './Random'
+export {default as Random } from './Random';
+export { default as Update } from './Update'
