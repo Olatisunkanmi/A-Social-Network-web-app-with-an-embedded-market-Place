221339d1c4b3dcb58936f842e61d3105ee795f44
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-28 17:14:55 +0100

    Update ProductCard.jsx

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -2,30 +2,31 @@ import { FavoriteOutlined, LocalGroceryStoreSharp } from '@material-ui/icons'
 import React from 'react'
 import img1 from '../Assests/M1.jpg'
 import PicArr from './Picture'
+import '../App.css'
 
 const ProductCard = () => {
     const random = PicArr[Math.floor(Math.random() * PicArr.length ) ]
 
   return (
-    <div className='bg-red-300 w-1/5 m-5'>
+    <div className=' w-1/5 m-5 pro-card'> 
 
-                         <div className='bg-white w-full rounded-lg h-full  flex flex-col justify-between'>
+                         <div className='bg-stone-300 w-full h-full  flex flex-col justify-between  '>
 
                                    
 
 
-                                                    <div className='flex flex-col rounded-lg h-5/6'>
-                                                            <img src={random } alt="" className='object-cover w-full rounded-t-lg h-4/5'/>
+                                                    <div className='flex flex-col   h-5/6 '>
+                                                            <img src={random } alt="" className='object-cover w-full  h-4/5'/>
                                                         
                                                             <p className='mt-3 text-center text-black font-serif font-bold text-lg'> Men Leather Jacket</p>
                                                             <p className=' leading-6 p-3 text-sm text-shade-800 font-sans font-semibold'> A Strong Leather Jacket for young men, who are fashionable and gentlemen. </p> 
                                                     </div>
 
 
-                                                    <div className=' px-5 h-1/6 flex flex-row justify-between items-center cursor-pointer'>
-                                                            <div className='flex flex-row items-center'>
-                                                                    <FavoriteOutlined  className='text-red-600'/>
-                                                                    <p className='text-md '> 456 </p>
+                                                    <div className=' px-5 h-1/6 flex flex-row justify-between items-center'>
+                                                            <div className='flex flex-row items-center bg-red-500 p-2  rounded-lg'>
+                                                                    <FavoriteOutlined  className='text-white'/>
+                                                                    <p className='text-md text-white'> 456 </p>
                                                             </div>
 
 
@@ -37,7 +38,7 @@ const ProductCard = () => {
                         
 
                        
-            </div>
+                         </div>
   
     </div>
   )
