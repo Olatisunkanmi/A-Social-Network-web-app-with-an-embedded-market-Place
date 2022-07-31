9ae3aaea8f7a005f539ede217e3cb68bcaae25a6
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-31 13:03:17 +0100

    Update Story.jsx

--- client/src/Components/Story.jsx
+++ client/src/Components/Story.jsx
@@ -6,7 +6,7 @@ import '../App.css'
 const story = () => {
   return (
     <div
-        className='story border-2 bg-white border-stone-300 rounded-xl mt-5 flex flex-col'>
+        className='story border-2 bg-white border-stone-300 rounded-xl flex flex-col'>
             <div 
                 className='flex-row flex justify-between p-2 px-3 '>
                     <p className='text-stone-500 font-semibold'> Stories </p>
@@ -76,45 +76,7 @@ const story = () => {
 
 
 
-            <div
-            className='border-t-2 border-stone-200 p-3 flex flex-row  items-center'>
-
-                    <div    className='mx-5 p-1 border-2 border-dotted rounded-full 
-                    cursor-pointer
-                    border-stone-600
-                    hover:border-solid
-                    hover:border-blue-600'>
-                    <img src={img} alt=""  className='border-2 m-1 border-black rounded-full h-10 w-10 object-cover'/>
-                     </div>
-
-
-                        <div className=''>
-                        <p className='font-mono font-semibold text-md'> Carl Robert. </p>
-                        <p  className='font-thin text-md'> 23 mins ago. </p>
-                        </div>
-               
-              </div>
-
-            
-
-            <div
-            className='border-t-2 border-stone-200 p-3 flex flex-row  items-center'>
-
-                    <div    className='mx-5 p-1 border-2 border-dotted rounded-full 
-                    cursor-pointer
-                    border-stone-600
-                    hover:border-solid
-                    hover:border-blue-600'>
-                    <img src={img} alt=""  className='border-2 m-1 border-black rounded-full h-10 w-10 object-cover'/>
-                     </div>
-
-
-                        <div className=''>
-                        <p className='font-mono font-semibold text-md'> Carl Robert. </p>
-                        <p  className='font-thin text-md'> 23 mins ago. </p>
-                        </div>
-               
-              </div>
+           
 
 
     </div>
