80c40c24cc03e3c496b64531908e6cbc4000b1c7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-31 13:03:10 +0100

    Create WeatherApi.jsx

---
+++ client/src/Components/WeatherApi.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+  import axios from "axios";
+import { LocationOn } from '@material-ui/icons';
+
+
+
+const WeatherApi = () => {
+
+  return (
+    <div>
+          <div  
+            className='p-5  text-center space-y-5 border-2 border-stone-300 rounded-xl mt-5'>
+              <p> 71 </p>
+              <p> Sunny </p> 
+              <p> Saturday, 18th 2018 </p> 
+              <p> <LocationOn /> Lagos City, Nigeria  </p>
+          </div>
+    </div>
+  )
+}
+
+export default WeatherApi
