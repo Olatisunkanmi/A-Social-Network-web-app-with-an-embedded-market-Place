f977515c450ca9562411526879b3d32844319667
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-09 21:57:18 +0100

    Create MoreVertIcon.jsx

---
+++ client/src/Components/MoreVertIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {  MoreVertOutlined } from '@material-ui/icons'
+import { useStateContext } from '../Contexts/Context'
+
+
+const MoreVertIcon = () => {
+      
+  return (
+    <div>
+    <button className=''   >
+                             <MoreVertOutlined />
+                          </button>
+    </div>
+  )
+}
+
+export default MoreVertIcon
