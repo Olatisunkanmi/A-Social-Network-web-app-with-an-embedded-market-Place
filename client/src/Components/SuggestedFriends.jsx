25e8759838dea35bf795e6928b302d0d83e1883c
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-09 21:57:07 +0100

    Update SuggestedFriends.jsx

--- client/src/Components/SuggestedFriends.jsx
+++ client/src/Components/SuggestedFriends.jsx
@@ -1,12 +1,13 @@
 import { Add, MoreVertOutlined } from '@material-ui/icons'
 import React from 'react'
-import img from '../Assests/pro.jpg'
+import {UserPicture } from './index'
+
 import '../App.css'
 
 const story = () => {
   return (
     <div
-        className='story border-2 bg-white border-stone-300 rounded-xl mt-5 flex flex-col'>
+        className='story border-2 bg-white border-stone-300 rounded-xl flex flex-col'>
             <div 
                 className='flex-row flex justify-between p-2 px-3 '>
                     <p className='text-stone-500 font-semibold'> Suggested Friends </p>
@@ -24,7 +25,7 @@ const story = () => {
                     border-stone-600
                     hover:border-solid
                     hover:border-blue-600'>
-                    <img src={img} alt=""  className='border-2 m-1 border-black rounded-full h-10 w-10 object-cover'/>
+                 <UserPicture />
                      </div>
 
 
@@ -35,58 +36,10 @@ const story = () => {
                      </div>
 
                      <Add />
-               </div>
+             
                
               </div>
 
-              
-
-
-            <div
-            className='border-t-2 border-stone-200 p-3 flex flex-row  items-center'>
-
-                    <div    className='mx-2 p-1  rounded-full 
-                    cursor-pointer
-                    border-stone-600
-                    hover:border-solid
-                    hover:border-blue-600'>
-                    <img src={img} alt=""  className='border-2 m-1 border-black rounded-full h-10 w-10 object-cover'/>
-                     </div>
-
-
-                     <div className='flex flex-row w-full justify-between'>
-                     <div className=''>
-                     <p className='font-medium font-sans text-md'> Drinkwater Skyline. </p>
-                     <p  className='font-thin text-xs'> Westham. </p>
-                     </div>
-
-                     <Add />
-               </div>
-               
-              </div>
-
-
-
-            <div
-            className='border-t-2 border-stone-200 p-3 flex flex-row  items-center'>
-
-                    <div    className='mx-2 p-1  rounded-full 
-                    cursor-pointer
-                    border-stone-600
-                    hover:border-solid
-                    hover:border-blue-600'>
-                    <img src={img} alt=""  className='border-2 m-1 border-black rounded-full h-10 w-10 object-cover'/>
-                     </div>
-
-
-                     <div className='flex flex-row w-full justify-between'>
-                     <div className=''>
-                     <p className='font-medium font-sans text-md'> Drinkwater Skyline. </p>
-                     <p  className='font-thin text-xs'> Westham. </p>
-                     </div>
-
-                     <Add />
-               </div>
                
               </div>
 
