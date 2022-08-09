bea3124475627fe4fc3a31557ab75a87d2919c42
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-09 21:57:29 +0100

    Create EachRecommend.jsx

---
+++ client/src/Components/EachRecommend.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {UserPicture } from './index'
+import {  Bookmark,} from '@material-ui/icons'
+
+
+const EachRecommend = () => {
+  return (
+    <div>
+                
+            <div
+            className='border-t-2 border-stone-200 p-3 flex flex-row  items-center'>
+
+                    <div    className='mx-2 p-1  rounded-full 
+                    cursor-pointer
+                    border-stone-600
+                    hover:border-solid
+                    hover:border-blue-600'>
+                  <UserPicture />
+                     </div>
+
+
+                     <div className='flex flex-row w-full justify-between'>
+                     <div className=''>
+                     <p className='font-medium font-sans text-md'> Family and Love </p>
+                     <p  className='font-thin text-xs'> Family. </p>
+                     </div>
+
+                     <button className='cursor-pointer'> 
+                        <Bookmark />
+                     </button>
+               </div>
+               
+              </div>
+
+
+
+    </div>
+  )
+}
+
+export default EachRecommend
