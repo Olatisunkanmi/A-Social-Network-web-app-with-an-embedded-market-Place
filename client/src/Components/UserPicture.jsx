723d3f8dd6e80b546d50382e43fadf98421e5166
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-09 22:32:54 -1200

    Update UserPicture.jsx

--- client/src/Components/UserPicture.jsx
+++ client/src/Components/UserPicture.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import img from '../Assests/pro.jpg'
-import '../App.css'
-
-
+import React from "react";
+import img from "../Assests/pro.jpg";
+import "../App.css";
 
 const UserPicture = () => {
   return (
     <div>
-    <img src={img} alt=""  className='m-1 rounded-full h-12 w-12 object-cover cursor-pointer'/>
+      <img
+        src={img}
+        alt=""
+        className="m-1 rounded-full h-12 w-12 object-cover cursor-pointer"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default UserPicture
+export default UserPicture;
