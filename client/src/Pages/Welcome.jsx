3338112e34165c9423b5c440a5fe13bd054a9cf0
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-09 22:33:19 -1200

    Update Welcome.jsx

--- client/src/Pages/Welcome.jsx
+++ client/src/Pages/Welcome.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import { Header } from '../Components'
-import '../App.css'
-import { useStateContext } from '../Contexts/Context';
-
-
-
+import React from "react";
+import { Header } from "../Components";
+import "../App.css";
+import { useStateContext } from "../Contexts/Context";
 
 const Welcome = () => {
-  const { sideBar, setSideBar } =  useStateContext();
+  const { sideBar, setSideBar } = useStateContext();
   return (
-    <div 
-    className={` text-black flex flex-col p-20 
-    ${sideBar ? 'w-3/5  m-auto' 
-        : 'w-9/12 m-auto' }`}>
-            <Header />
-            <p>  TIME LINE</p>
-          </div>
-  )
-  
-}
+    <div
+      className={` text-black flex flex-col p-20 
+    ${sideBar ? "w-3/5  m-auto" : "w-9/12 m-auto"}`}
+    >
+      <Header />
+      <p> TIME LINE</p>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
