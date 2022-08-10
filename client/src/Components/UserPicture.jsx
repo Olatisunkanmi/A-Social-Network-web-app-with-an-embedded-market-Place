8c7897c4a0d45c1bacf4ca5aea33af5f81e9609b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-10 04:52:59 +0100

    Update UserPicture.jsx

--- client/src/Components/UserPicture.jsx
+++ client/src/Components/UserPicture.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import img from '../Assests/pro.jpg'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 
 
 const UserPicture = () => {
   return (
     <div>
+      <Link to='/444'>
     <img src={img} alt=""  className='m-1 rounded-full h-12 w-12 object-cover cursor-pointer'/>
+
+      </Link>
     </div>
   )
 }
