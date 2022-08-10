ef3dce43f3f8f8a15c90a053300d50e1dbcd9090
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-09 22:33:15 -1200

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,54 +1,44 @@
-import React from 'react';
-import {  Upload, Story, Random, Update, SuggestedFriends ,RecommendedPages , WeatherApi } from '../Components';
-import { useStateContext } from '../Contexts/Context';
-import '../App.css'
+import React from "react";
+import {
+  Upload,
+  Story,
+  Random,
+  Update,
+  SuggestedFriends,
+  RecommendedPages,
+  WeatherApi,
+} from "../Components";
+import { useStateContext } from "../Contexts/Context";
+import "../App.css";
 
 const Timeline = () => {
-  const { sideBar, setSideBar } =  useStateContext();
+  const { sideBar, setSideBar } = useStateContext();
+
   return (
-    <div 
-    className=''>
-      
-                  <div className='m-auto 
+    <div className="">
+      <div
+        className="m-auto w-4/5 xl:w-4/5 lg:w-full flex flex-col 
+lg:flex-row justify-center p-5 mt-5"
+      >
+        <div className="hidden lg:block lg:w-1/4">
+          <RecommendedPages />
+          <WeatherApi />
+        </div>
+        {/** Feed Section  */}
+        <div className="xl:block lg:w-2/4 lg:mx-5">
+          <Upload />
+          <Update />
+          <Update />
+        </div>
+        {/** Stories Section  */}
+        <div className="lg:w-1/4">
+          <Story />
+          <Random />
+          <SuggestedFriends />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                    w-4/5
-                    xl:w-4/5
-                  lg:w-full flex flex-col 
-                  lg:flex-row justify-center p-5 mt-5'>
-
-                        <div className='hidden 
-                        lg:block
-                          lg:w-1/4'>
-                      
-                        <RecommendedPages />
-                        <WeatherApi />
-                        </div>
-
-                          {/** Feed Section  */}
-                              <div 
-                                className='
-                                xl:block
-                               
-                                lg:w-2/4 
-                                lg:mx-5'>
-                                  <Upload />
-                                  <Update />
-                                  <Update />
-                              </div>
-
-
-                          {/** Stories Section  */}
-
-
-                              <div className='
-                              lg:w-1/4 '>
-                                  <Story />
-                                  <Random />
-                                  <SuggestedFriends />
-                              </div>
-                  </div>
-          </div>
-  )
-}
-
-export default Timeline
+export default Timeline;
