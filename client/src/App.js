315db5fd3d7d8b91bcc9212277843dc0341e661e
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-09 22:29:38 -1200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { useState } from 'react';
-import {Home} from './Pages/index'
-import { useStateContext } from './Contexts/Context';
-import { Welcome, Timeline,  Header} from './Components/index'
-import {Products} from './Pages/index'
-
+import { useState } from "react";
+import { Home } from "./Pages/index";
+import { useStateContext } from "./Contexts/Context";
+import { Welcome, Timeline, Header } from "./Components/index";
+import { Products } from "./Pages/index";
 
 function App() {
   const { sideBar, setSideBar } = useStateContext();
-    console.log(sideBar)
+  console.log(sideBar);
   return (
-   
-      <div className= ''>
-         <Header />
-          <Routes>
-            <Route exact path='/timeline' element={ <Timeline />} />
-            <Route exact path='/friends' element={ <Welcome />} />
-            <Route exact path='/products' element={ <Products />} />
-          </Routes>
-      </div>
-   
+    <div className="">
+      <Header />
+      <Routes>
+        <Route exact path="/timeline" element={<Timeline />} />
+        <Route exact path="/friends" element={<Welcome />} />
+        <Route exact path="/products" element={<Products />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
