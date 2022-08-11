77a2facec9330f6bc5f05963f1c80d83bfc03778
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-11 09:45:40 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
 		<div className='bg-neutral-100'>
 			<Header />
 			<Routes>
-				<Route exact path='/:id' element={<ProfileMain />} />
 				<Route exact path='/timeline' element={<Timeline />} />
 				<Route exact path='/friends' element={<Welcome />} />
+				<Route exact path='/profile/:id' element={<ProfileMain />} />
 				<Route exact path='/products' element={<Products />} />
 			</Routes>
 		</div>
