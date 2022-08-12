39ae593eccca21670b5f2abc17add974984509c7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-12 01:03:02 +0100

    Update Cart.jsx

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -3,21 +3,22 @@ import {
 	CartProduct,
 	OrderDetails,
 	HelpDesk,
+	CartProductSmallDevices,
 } from '../Components/index';
 
 const Cart = () => {
 	return (
 		<div className='mt-10 m-auto lg:w-4/5 xl:w-3/5 '>
 			{/**   Cart Header  */}
-			<div className=' justify-between w-full flex flex-row items-center '>
+			<div className=' justify-between w-full flex flex-row items-center p-5 md:p-0'>
 				<div>
 					<p className='text-2xl font-semibold font-serif'>My Cart</p>
 				</div>
 
-				<div className='flex flex-row items-center space-x-3'>
+				<div className='flex flex-row items-center space-x-3 text-sm'>
 					<p className='font-semibold'>save order </p>
 
-					<button className='py-2 px-4 rounded-xl text-white font-semibold hover:bg-black bg-blue-600'>
+					<button className='py-2 px-4 rounded-xl text-white font-semibold hover:bg-blue-400 bg-blue-600'>
 						Checkout
 					</button>
 				</div>
@@ -25,20 +26,22 @@ const Cart = () => {
 
 			{/* Cart Section *  */}
 
-			<div className='w-full flex flex-row mt-10 justify-between space-x-10'>
-				<div className='w-4/6 '>
+			<div className='w-full flex flex-col lg:flex-row mt-10 justify-between md:space-x-10'>
+				<div className='w-4/6 hidden md:block'>
 					<CartProduct />
 				</div>
 
-				<div className='w-2/6 '>
-					<div className=''>
-						<p className='text-md font-semibold text-neutral-600'>
-							{' '}
-							Order Summary{' '}
-						</p>
-						<OrderDetails />
-						<HelpDesk />
-					</div>
+				<div className='w-full block md:hidden p-3 space-y-3'>
+					<CartProductSmallDevices />
+					<CartProductSmallDevices />
+				</div>
+
+				<div className='w-full space-y-2 flex flex-col p-5 md:p-0 md:w-2/6 '>
+					<p className='text-md font-semibold text-neutral-600'>
+						Order Summary
+					</p>
+					<OrderDetails />
+					<HelpDesk />
 				</div>
 			</div>
 		</div>
