c9f01cb79dc8f0839f4666e8b30fa743b55914ce
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-12 03:39:46 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-
 import { Welcome, Header } from './Components/index';
-import { Products, Timeline, ProfileMain, Cart } from './Pages/index';
+import {
+	Products,
+	Timeline,
+	ProfileMain,
+	Cart,
+	CheckOut,
+} from './Pages/index';
 
 function App() {
 	return (
@@ -12,7 +17,8 @@ function App() {
 				<Route exact path='/timeline' element={<Timeline />} />
 				<Route exact path='/friends' element={<Welcome />} />
 				<Route exact path='/profile/:id' element={<ProfileMain />} />
-				<Route exact path='/profile/cart/:id' element={<Cart />} />
+				<Route exact path='/cart/:id' element={<Cart />} />
+				<Route exact path='/checkout/:id' element={<CheckOut />} />
 				<Route exact path='/products/:id' element={<Products />} />
 			</Routes>
 		</div>
