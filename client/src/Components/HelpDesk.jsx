b09356634ae3f5c274f08505af714d6b11e637df
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-12 01:02:54 +0100

    Update HelpDesk.jsx

--- client/src/Components/HelpDesk.jsx
+++ client/src/Components/HelpDesk.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import help from '../Assests/help.svg';
 
 const HelpDesk = () => {
-	return <div>HelpDesk</div>;
+	return (
+		<div className='border-2 bg-white border-neutral-400 rounded-2xl w-full p-6 items-center'>
+			<img src={help} alt='' className='w-1/2 ' />
+
+			<p className=' font-semibold'>Help center</p>
+
+			<p>
+				{' '}
+				Having trouble? Please search our Help Center for a quick
+				answer to your problem
+			</p>
+		</div>
+	);
 };
 
 export default HelpDesk;
