e2f4ab53289030e762017fa404241d0221ae4923
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-13 04:49:53 +0100

    Create ContactCard.jsx

---
+++ client/src/Components/ContactCard.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const ContactCard = () => {
+	return (
+		<div className='space-y-5'>
+			<div className=''>
+				<p className='text-ms font-serif text-black font-semibold'>
+					Contact Information
+				</p>
+			</div>
+
+			<div className='mt-3'>
+				<p className='text-neutral-500 text-xs font-bold'>
+					Email Address
+				</p>
+				<input
+					type='text'
+					className='payment-input'
+					placeholder='Enter your email address'
+				/>
+			</div>
+
+			<p className='text-ms font-serif text-black font-semibold mt-5'>
+				Payment Information{' '}
+			</p>
+
+			<div>
+				<p className='text-neutral-500 text-xs font-bold'>
+					Name on Card{' '}
+				</p>
+				<input
+					type='text'
+					placeholder='Enter the name on the credit card'
+					className='payment-input'
+				/>
+			</div>
+			<p className='text-neutral-500 text-xs font-bold'>
+				{' '}
+				CREDIT OR DEBIT CARD
+			</p>
+			<div className='flex flex-row payment-input '>
+				<input
+					type='text'
+					placeholder='Card number '
+					className='w-4/5 outline-none placeholder:text-xs'
+				/>
+				<input
+					type='text'
+					placeholder='MM/YY  '
+					className='w-1/5 outline-none placeholder:text-xs'
+				/>
+				<input
+					type='text'
+					placeholder=' CVC'
+					className='w-1/5  outline-none placeholder:text-xs'
+				/>
+			</div>
+
+			<div>
+				<p className='text-neutral-500 text-xs font-bold'> COUNTRY</p>
+				<select className='payment-input'>
+					<option> United States of America </option>
+					<option> Nigeria </option>
+				</select>
+			</div>
+
+			<div>
+				<p className='text-neutral-500 text-xs font-bold'>
+					By clicking on 'Submit Payment', you confirm that you fully
+					agree to our Terms Of Service and to our Customer's Policy.
+				</p>
+			</div>
+
+			<div className='text-center'>
+				<button className='p-5 bg-blue-600 text-white rounded-xl payment-btn font-semibold font-sans '>
+					<p>
+						Confrim Payment <span>$200 </span>{' '}
+					</p>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ContactCard;
