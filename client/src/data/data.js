d0f756945024ad1bad86f6092e74ca5723a82054
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-13 01:29:03 -1200

    Update data.js

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,4 +1,25 @@
 import imageIon from "../Assests/pro.jpg";
+import img1 from "../Assests/1.svg";
+import img2 from "../Assests/2.svg";
+import img3 from "../Assests/3.svg";
+import img4 from "../Assests/4.svg";
+import img5 from "../Assests/5.svg";
+import img6 from "../Assests/6.svg";
+import img7 from "../Assests/7.svg";
+import img8 from "../Assests/8.svg";
+import img9 from "../Assests/9.svg";
+import img10 from "../Assests/10.svg";
+import img11 from "../Assests/11.svg";
+import img12 from "../Assests/12.svg";
+import img13 from "../Assests/13.svg";
+import img14 from "../Assests/14.svg";
+import img15 from "../Assests/15.svg";
+import img16 from "../Assests/16.svg";
+import img17 from "../Assests/17.svg";
+import img18 from "../Assests/18.svg";
+import img19 from "../Assests/19.svg";
+import img20 from "../Assests/20.svg";
+import img21 from "../Assests/21.svg";
 import { ChatBubble, FavoriteBorder, LiveTv, Image } from "@material-ui/icons";
 
 export const notificationData = [
@@ -37,5 +58,155 @@ export const notificationData = [
     extras: "you and 2 other people",
     time: "2 days ago",
     icon: <Image />,
+  },
+];
+
+export const productsData = [
+  {
+    img: img1,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img2,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img3,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img4,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img5,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img6,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img7,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img8,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img9,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img10,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img11,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img12,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img13,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img14,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img15,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img16,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img17,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img18,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img19,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img20,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
+  },
+  {
+    img: img21,
+    name: "The T-Shirt",
+    text: "A nice t-shirt that goes everywhere with anything, try it.",
+    likes: 268,
+    price: 18.0,
   },
 ];
