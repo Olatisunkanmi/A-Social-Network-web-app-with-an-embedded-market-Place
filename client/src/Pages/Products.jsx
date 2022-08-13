e3c1db36d823259f92fba48a4d157581a948389b
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-12 23:53:33 -1200

    Add files via upload

--- client/src/Pages/Products.jsx
+++ client/src/Pages/Products.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import {
-	ProductHeader,
-	ProductGrid,
-	ProductCard,
-} from '../Components';
-import '../App.css';
+import React from "react";
+import { ProductHeader, ProductGrid, ProductCard } from "../Components";
+import "../App.css";
 
 const Products = () => {
-	return (
-		<div
-			className={` text-black flex flex-col bg-gray-400 
+  return (
+    <div
+      className={` text-black flex flex-col bg-gray-400 
       w-full`}
-		>
-			<div className='w-4/5 m-auto '>
-				<ProductGrid />
-				<div className='flex flex-row flex-wrap w-full justify-evenly'>
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-				</div>
-			</div>
-		</div>
-	);
+    >
+      <div className="w-4/5 m-auto ">
+        <ProductGrid />
+        <div className="grid grid-cols-2 w-full">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
