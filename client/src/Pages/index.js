b0a422747a3bcf0eec78126db70328a0d978cdf7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-13 04:49:40 +0100

    Update index.js

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -4,3 +4,4 @@ export { default as Timeline } from './Timeline';
 export { default as ProfileMain } from './ProfileMain';
 export { default as Cart } from './Cart';
 export { default as CheckOut } from './CheckOut';
+export { default as Payment } from './Payment';
