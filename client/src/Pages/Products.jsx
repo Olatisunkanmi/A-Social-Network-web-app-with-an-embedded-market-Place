ad37010c3583af99a6b370ddc0cd17e672d83e11
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-13 01:29:15 -1200

    Update Products.jsx

--- client/src/Pages/Products.jsx
+++ client/src/Pages/Products.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { ProductHeader, ProductGrid, ProductCard } from "../Components";
+import { useStateContext } from "../Contexts/Context";
 import "../App.css";
 
 const Products = () => {
+  const value = useStateContext();
+
   return (
-    <div
-      className={` text-black flex flex-col bg-gray-400 
-      w-full`}
-    >
-      <div className="w-4/5 m-auto ">
+    <div className={`text-black flex flex-col bg-gray-400 w-full`}>
+      <div className="w-4/5 m-auto">
         <ProductGrid />
-        <div className="grid grid-cols-2 w-full">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-6">
+          {value.productState.product.map((item, _index) => {
+            return <ProductCard item={item} />;
+          })}
         </div>
       </div>
     </div>
