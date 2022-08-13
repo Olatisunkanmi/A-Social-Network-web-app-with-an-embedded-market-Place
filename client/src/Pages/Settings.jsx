420d0bfee50505bb5424b1d5f1ae7b34d79a009a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-13 13:28:26 +0100

    Create Settings.jsx

---
+++ client/src/Pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SettingsSidebar, Sidebar } from '../Components/index';
+
+const Settings = () => {
+	return (
+		<div>
+			<Sidebar />
+		</div>
+	);
+};
+
+export default Settings;
