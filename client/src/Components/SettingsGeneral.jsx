b3be6fc310ff252f4416bb07a33a0c0b31052f47
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-14 07:38:00 +0100

    Create SettingsGeneral.jsx

---
+++ client/src/Components/SettingsGeneral.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsGeneral = () => {
+	return (
+		<div className='lg:3/5'>
+			<div>Hello World</div>
+		</div>
+	);
+};
+
+export default SettingsGeneral;
