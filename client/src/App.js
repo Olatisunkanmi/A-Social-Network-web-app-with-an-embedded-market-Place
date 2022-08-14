cbed8e0fb4f78663e0911b0bae22c5008470ecce
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-14 05:03:17 -1200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Welcome, Header } from "./Components/index";
+import { Welcome, Header, Sidebar } from "./Components/index";
 import {
   Products,
   Timeline,
@@ -9,12 +9,15 @@ import {
   CheckOut,
   Payment,
   Settings,
-} from "./Pages/index";
+} from "./Pages";
+import { useState } from "react";
 
 function App() {
+  const [sidebar, setSidebar] = useState(false);
+
   return (
     <div className="bg-neutral-100">
-      <Header />
+      <Header setSidebar={setSidebar} />
       <Routes>
         <Route exact path="/" element={<Timeline />} />
         <Route exact path="/friends" element={<Welcome />} />
@@ -25,6 +28,13 @@ function App() {
         <Route exact path="/products/:id" element={<Products />} />
         <Route exact path="/settings" element={<Settings />} />
       </Routes>
+      <aside
+        className={`fixed ${
+          sidebar ? "right-0" : "-right-full"
+        } top-0 bg-white flex flex-col h-screen w-3/4 sm:w-2/4 px-6 py-2 md:hidden z-50 transition-all duration-700`}
+      >
+        <Sidebar setSidebar={setSidebar} />
+      </aside>
     </div>
   );
 }
