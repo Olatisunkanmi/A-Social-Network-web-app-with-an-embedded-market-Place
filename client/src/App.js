4ec4486a5bed900987c6c42bbb0c933f5babb21e
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-14 03:15:53 -1200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Welcome, Header } from './Components/index';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Welcome, Header } from "./Components/index";
 import {
-	Products,
-	Timeline,
-	ProfileMain,
-	Cart,
-	CheckOut,
-	Payment,
-	Settings,
-} from './Pages/index';
+  Products,
+  Timeline,
+  ProfileMain,
+  Cart,
+  CheckOut,
+  Payment,
+  Settings,
+} from "./Pages/index";
 
 function App() {
-	return (
-		<div className='bg-neutral-100'>
-			<Header />
-			<Routes>
-				<Route exact path='/timeline' element={<Timeline />} />
-				<Route exact path='/friends' element={<Welcome />} />
-				<Route exact path='/profile/:id' element={<ProfileMain />} />
-				<Route exact path='/cart/:id' element={<Cart />} />
-				<Route exact path='/checkout/:id' element={<CheckOut />} />
-				<Route exact path='/payment' element={<Payment />} />
-				<Route exact path='/products/:id' element={<Products />} />
-				<Route exact path='/settings' element={<Settings />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="bg-neutral-100">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Timeline />} />
+        <Route exact path="/friends" element={<Welcome />} />
+        <Route exact path="/profile/:id" element={<ProfileMain />} />
+        <Route exact path="/cart/:id" element={<Cart />} />
+        <Route exact path="/checkout/:id" element={<CheckOut />} />
+        <Route exact path="/payment" element={<Payment />} />
+        <Route exact path="/products/:id" element={<Products />} />
+        <Route exact path="/settings" element={<Settings />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
