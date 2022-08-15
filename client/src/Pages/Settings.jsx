6562814f12894581b5faf6e6d7d3ec381c81b52a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-15 01:41:45 +0100

    Update Settings.jsx

--- client/src/Pages/Settings.jsx
+++ client/src/Pages/Settings.jsx
@@ -5,19 +5,20 @@ import {
 	SettingsSecurity,
 } from '../Components/index';
 import { Route, Routes } from 'react-router-dom';
+import '../App.css';
 
 const Settings = () => {
 	return (
-		<div className='w-full flex flex-row'>
-			<SettingsSidebar />
-			<Routes>
-				<Route exact path='/' element={<SettingsGeneral />} />
-				<Route
-					exact
-					path='/security'
-					element={<SettingsSecurity />}
-				/>
-			</Routes>
+		<div className='w-full flex flex-row '>
+			<div className=' hidden border-2 border-neutral-500 border-y-0  md:w-3/12 xl:w-2/12'>
+				<SettingsSidebar />
+			</div>
+
+			<div className='p-10 md:w-8/12 xl:w-9/12 m-auto w-full'>
+				<Routes>
+					<Route exact path='/' element={<SettingsGeneral />} />
+				</Routes>
+			</div>
 		</div>
 	);
 };
