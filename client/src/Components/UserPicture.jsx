e061218702e5264a509e5f3419e39abe7830bfe2
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-16 03:15:42 +0100

    Update UserPicture.jsx

--- client/src/Components/UserPicture.jsx
+++ client/src/Components/UserPicture.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import img from "../Assests/pro.jpg";
-import "../App.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import img from '../Assests/pro.jpg';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 const UserPicture = () => {
-  return (
-    <div>
-      <Link to="/profile/12">
-        <img
-          src={img}
-          alt=""
-          height="40px"
-          width="40px"
-          className="mx-1 rounded-full object-cover cursor-pointer"
-        />
-      </Link>
-    </div>
-  );
+	return (
+		<div>
+			<Link to='/profile/12'>
+				<img
+					src={img}
+					alt=''
+					height='35px'
+					width='40px'
+					className='mx-1 rounded-full object-cover cursor-pointer'
+				/>
+			</Link>
+		</div>
+	);
 };
 
 export default UserPicture;
