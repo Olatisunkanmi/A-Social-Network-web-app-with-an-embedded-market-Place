e336799eda1b86f3c2f998b24e1bfa40ea10bf09
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-20 17:41:36 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,12 @@
+const express = require('express');
 const POSTROUTER = require('./routes/PostsRoute');
 const USERROUTER = require('./routes/UserRoutes');
 const PRODUCTSROUTER = require('./routes/ProductsRoutes');
-
-const express = require('express');
-require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-app.use('/api/v1/posts/', POSTROUTER);
-app.use('/api/v1/users/', USERROUTER);
-app.use('/api/v1/products/', PRODUCTSROUTER);
+app.use('/api/v1/posts', POSTROUTER);
+app.use('/api/v1/users', USERROUTER);
+app.use('/api/v1/products', PRODUCTSROUTER);
 
-// 	------------------- PORT CONNECTION
-const port = process.env.Port || 2000;
-app.listen(port, () => {
-	console.log(`Node   ${port}`);
-});
+module.exports = app;
