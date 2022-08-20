a06d956069b3b0e99893acb0cfe1a1bf4e5d9b2a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-21 00:35:17 +0100

    Create UserController.js

---
+++ api/Controllers/UserController.js
@@ -0,0 +1,41 @@
+// 	-------------CREATinig Second APi ---User
+// !  CREATE MIDDLEWARE TO CHECK FOR :ID
+
+// 			---------Get all Users ?
+exports.getAllUsers = (req, res) => {
+	res.status(500).json({
+		status: 'Error',
+		message: 'Users absent',
+	});
+};
+// ----------------Create USer
+exports.createUser = (req, res) => {
+	res.status(500).json({
+		status: 'Error',
+		message: 'Users absent',
+	});
+};
+
+// 	----------GET  A User
+exports.getUser = (req, res) => {
+	res.status(500).json({
+		status: 'Error',
+		message: 'Users absent',
+	});
+};
+
+// -------------Update a User
+exports.updateUser = (req, res) => {
+	res.status(500).json({
+		status: 'Error',
+		message: 'Users absent',
+	});
+};
+
+// 	----------Delete  A User
+exports.deleteUser = (req, res) => {
+	res.status(500).json({
+		status: 'Error',
+		message: 'Users absent',
+	});
+};
