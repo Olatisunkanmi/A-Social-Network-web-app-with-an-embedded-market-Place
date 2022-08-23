6f135a06963f4fe072718a98620465d93074dd8e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-23 20:15:55 +0100

    Update ProductsRoutes.js

--- api/routes/ProductsRoutes.js
+++ api/routes/ProductsRoutes.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../Controllers/ProductsController');
-// const {
-// 	getAllProducts,
-// } = require('../Controllers/ProductsController');
-// const { getAllProducts } = require('../Controllers/FakeMiddleware  ');
-const FakeMiddleWare = require('../Controllers/FakeMiddleware');
 
-// router.route('/').get(ProductController.getAllProducts);
+const FakeMiddleWare = require('../Controllers/FakeMiddleware');
 
 console.log(typeof FakeMiddleWare.CHECK);
 router.route('/').get(FakeMiddleWare.CHECK, FakeMiddleWare.CH);
