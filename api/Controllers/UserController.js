913c3884841300b1ef09276abebfd02791f7a528
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-23 20:15:46 +0100

    Update UserController.js

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -1,11 +1,19 @@
+const fs = require('fs');
+
+const Users = JSON.parse(
+	fs.readFileSync(`${__dirname}/../data/user.json`),
+);
+
 // 	-------------CREATinig Second APi ---User
+
 // !  CREATE MIDDLEWARE TO CHECK FOR :ID
 
 // 			---------Get all Users ?
 exports.getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'Error',
-		message: 'Users absent',
+	res.status(200).json({
+		status: 'success',
+		resuilt: Users.length,
+		Data: Users,
 	});
 };
 // ----------------Create USer
@@ -18,9 +26,14 @@ exports.createUser = (req, res) => {
 
 // 	----------GET  A User
 exports.getUser = (req, res) => {
-	res.status(500).json({
-		status: 'Error',
-		message: 'Users absent',
+	let id = req.params.id * 1;
+	console.log(id);
+	let user = Users.find((el) => el.index === id);
+	console.log(user);
+	res.status(200).json({
+		status: 'success',
+		resuilt: user.length,
+		Data: user,
 	});
 };
 
