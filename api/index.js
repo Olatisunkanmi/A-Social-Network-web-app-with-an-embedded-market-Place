2196cfb72041a883518be523af4f4175a8227dde
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-25 14:27:53 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const PRODUCTSROUTER = require('./routes/ProductsRoutes');
 const app = express();
 app.use(express.json());
 
-app.use('/api/v1/posts', POSTROUTER);
+// app.use('/api/v1/posts', POSTROUTER);
 app.use('/api/v1/users', USERROUTER);
-// app.use('/api/v1/products', PRODUCTSROUTER);
+app.use('/api/v1/products', PRODUCTSROUTER);
 
 module.exports = app;
