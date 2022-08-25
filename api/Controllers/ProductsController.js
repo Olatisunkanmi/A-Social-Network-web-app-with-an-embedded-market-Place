b483b1165f8ca45430e4b2332fa08b4bc7b544cb
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-25 14:28:06 +0100

    Update ProductsController.js

--- api/Controllers/ProductsController.js
+++ api/Controllers/ProductsController.js
@@ -1,17 +1,37 @@
-// ----Creating our third api
+const Product = require('../Models/ProductModel');
 
 // Get all porducts
-exports.getAllProducts = (req, res) => {
-	console.log('TT');
-	res.status(500).json({
-		status: 'Erro r',
-		message: ' Product Absent',
-	});
+exports.getAllProducts = async (req, res) => {
+	try {
+		const Products = await Product.find();
+		res.status(200).json({
+			status: 'Success',
+			data: {
+				products: Products,
+			},
+		});
+	} catch (error) {
+		res.status(404).json({
+			status: 'Fail',
+			message: error,
+		});
+	}
 };
 
-exports.createProducts = (req, res) => {
-	res.status(500).json({
-		status: ' Error ',
-		message: 'Product Not Created',
-	});
+exports.createProducts = async (req, res) => {
+	try {
+		const NewProduct = await Product.create(req.body);
+		res.status(200).json({
+			status: 'Success',
+			message: 'Product Created',
+			data: {
+				Product: NewProduct,
+			},
+		});
+	} catch (error) {
+		res.status(404).json({
+			status: 'Fail',
+			message: error,
+		});
+	}
 };
