cd9c0c64f891f7ebcdb6377ec2390e11bdbca97c
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-04 04:50:30 -1200

    Update Timeline.jsx

--- client/src/Pages/Timeline.jsx
+++ client/src/Pages/Timeline.jsx
@@ -1,65 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
-	Upload,
-	Story,
-	Random,
-	Update,
-	SuggestedFriends,
-	RecommendedPages,
-	WeatherApi,
-} from '../Components';
-import '../App.css';
+  Upload,
+  Story,
+  Random,
+  Update,
+  SuggestedFriends,
+  RecommendedPages,
+  WeatherApi,
+  InputModal,
+} from "../Components";
+import "../App.css";
 
 const Timeline = () => {
-	return (
-		<div className=''>
-			<div
-				className='md:m-auto
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div>
+      {openModal && (
+        <InputModal setOpenModal={setOpenModal} openModal={openModal} />
+      )}
+      <div
+        className="md:m-auto
                     w-full
                     xl:w-3/4
                    flex flex-col 
 				   space-y-5
-                  lg:flex-row justify-center md:p-5 mt-5'
-			>
-				<div
-					className='hidden 
+                  lg:flex-row justify-center md:p-5 mt-5"
+      >
+        <div
+          className="hidden 
                                       space-y-5
                                       lg:block
-                                        lg:w-1/4'
-				>
-					<RecommendedPages />
-					<WeatherApi />
-				</div>
+                                        lg:w-1/4"
+        >
+          <RecommendedPages />
+          <WeatherApi />
+        </div>
 
-				{/** Feed Section  */}
-				<div
-					className='
+        {/** Feed Section  */}
+        <div
+          className="
 								
                                 xl:block
                                space-y-5
                                 lg:w-2/4 
-                                lg:mx-5'
-				>
-					<Upload />
-					<Update />
-					<Update />
-				</div>
+                                lg:mx-5"
+        >
+          <Upload setOpenModal={setOpenModal} openModal={openModal} />
+          <Update />
+          <Update />
+        </div>
 
-				{/** Stories Section  */}
+        {/** Stories Section  */}
 
-				<div
-					className='
+        <div
+          className="
 
                             space-y-5
-                              lg:w-1/4 '
-				>
-					<Story />
-					<Random />
-					<SuggestedFriends />
-				</div>
-			</div>
-		</div>
-	);
+                              lg:w-1/4 "
+        >
+          <Story />
+          <Random />
+          <SuggestedFriends />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
