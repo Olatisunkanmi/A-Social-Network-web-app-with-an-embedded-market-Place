8a34aaacde3c2ce361a69f56c4f1d29bfbfc8d66
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-06 15:45:46 +0100

    Update ProductsController.js

--- api/Controllers/ProductsController.js
+++ api/Controllers/ProductsController.js
@@ -1,19 +1,64 @@
+const { ifError } = require('assert');
 const Product = require('../Models/ProductModel');
 
-// Get all porducts
+exports.Clothing = async (req, res, next) => {
+	req.query.category = 'Clothing';
+
+	next();
+};
+
+// Get top 5 products
+exports.getHighPrices = async (req, res, next) => {
+	req.query.sort = '-price';
+	next();
+};
+exports.getLowPrices = async (req, res, next) => {
+	req.query.sort = 'price';
+	next();
+};
+
+exports.getRating = async (req, res, next) => {
+	req.query.sort = '-ratingsAverage';
+	next();
+};
+// Get all products
 exports.getAllProducts = async (req, res) => {
 	try {
-		const Products = await Product.find();
+		// build query methods
+		// 1a) Filter query
+		const queryObj = { ...req.query };
+		let excludedFields = ['limit', 'sort', 'page', 'fields'];
+		excludedFields.forEach((cur) => delete queryObj[cur]);
+
+		// ! query to find in the DB
+		let query = Product.find(queryObj);
+
+		// 1b)  adv filter
+
+		// 2. using sort
+		if (req.query.sort) {
+			console.log('jj');
+			console.log(typeof req.query.sort);
+			let sortBy = req.query.sort.split(',').join(' ');
+			console.log(sortBy);
+			query = query.sort(req.query.sort);
+		} else {
+			query = query.sort('-createdAt');
+		}
+		const products = await query;
+
+		// const products = await Product.find();
 		res.status(200).json({
 			status: 'Success',
+			result: products.length,
 			data: {
-				products: Products,
+				products: products,
 			},
 		});
 	} catch (error) {
 		res.status(404).json({
 			status: 'Fail',
-			message: error,
+			message: error.message,
 		});
 	}
 };
@@ -31,7 +76,7 @@ exports.createProducts = async (req, res) => {
 	} catch (error) {
 		res.status(404).json({
 			status: 'Fail',
-			message: error,
+			message: error.message,
 		});
 	}
 };
